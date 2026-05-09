62fe0519fc2efb3a2563ce05569a4168c52d1f18
alykay1522 <alykay1522@users.noreply.github.com>
2026-05-09 10:09:47 -0500

    Update _sim167.js

--- _sim167.js
+++ _sim167.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 let s = fs.readFileSync(__dirname + "/package.json", "utf8");
-s = s.replace('"vue": "^3.3.0"\r\n  },', '"vue": "^3.3.0",\r\n  },');
+s = s.replace('"preview": "vite preview"', '"preview": vite preview');
 try {
   JSON.parse(s);
 } catch (e) {
