443e62bbd576ad3592f4682b7d7605a1a4fa1749
alykay1522 <alyshameade.1522@gmail.com>
2026-06-27 10:40:41 -0500

    feat: combine cleaned catalog recovery batches

---
+++ artifacts/api-server/src/data/catalogSeed.ts
@@ -0,0 +1,17 @@
+import batch0 from "./catalogSeed0.json";
+import batch1 from "./catalogSeed1.json";
+import batch2 from "./catalogSeed2.json";
+import batch3 from "./catalogSeed3.json";
+import batch4 from "./catalogSeed4.json";
+import batch5a from "./catalogSeed5a.json";
+import batch5b from "./catalogSeed5b.json";
+
+export const cleanedCatalogSeed = [
+  ...batch0,
+  ...batch1,
+  ...batch2,
+  ...batch3,
+  ...batch4,
+  ...batch5a,
+  ...batch5b,
+];
