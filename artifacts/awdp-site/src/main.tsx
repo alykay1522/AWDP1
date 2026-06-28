cc3cad67995ca4fe90fdace64fe6087ab9a04cea
alykay1522 <alyshameade.1522@gmail.com>
2026-06-28 17:43:18 -0500

    fix: load responsive banner and interaction overrides

--- artifacts/awdp-site/src/main.tsx
+++ artifacts/awdp-site/src/main.tsx
@@ -2,5 +2,6 @@ import "./installApiBaseUrl.js";
 import { createRoot } from "react-dom/client";
 import App from "./AppEnhanced.jsx";
 import "./index.css";
+import "./site-fixes.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
